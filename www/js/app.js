7005e96a58f0d72bcf83a972d7e19785b9955aa2
James Socol <james@mozilla.com>
2013-01-25 14:02:58 -0500

    Working (around bugs).

--- www/js/app.js
+++ www/js/app.js
@@ -12,42 +12,76 @@ define(function(require) {
     // Install the layouts
     require('layouts/layouts');
 
-    // Write your app here.
-
-    function formatDate(d) {
-        return (d.getMonth()+1) + '/' +
-            d.getDate() + '/' +
-            d.getFullYear();
-     }
-
     // Passing a function into $ delays the execution until the
     // document is ready
     $(function() {
 
         // List view
         var list = $('.list').get(0);
-        list.add({ title: 'Cook yummy food',
-                   desc: 'COOK ALL THE THINGS',
-                   date: new Date() });
-        list.add({ title: 'Make things',
-                   desc: 'Make this look like that',
-                   date: new Date(12, 9, 5) });
-        list.add({ title: 'Move stuff',
-                   desc: 'Move this over there',
-                   date: new Date(12, 10, 1) });
-        list.add({ title: 'Cook yummy food',
-                   desc: 'COOK ALL THE THINGS',
-                   date: new Date() });
-        list.add({ title: 'Make things',
-                   desc: 'Make this look like that',
-                   date: new Date(12, 9, 5) });
-
-        list.nextView = 'x-view.details';
+        list.nextView = 'x-view.detail';
 
         // Detail view
-        var details = $('.details').get(0);
-        details.render = function(item) {
+        var detail = $('.detail').get(0);
+        detail.render = function(item) {
+            window.item = item;
+            var ingredients = (item.get('ingredients') || '').split(/\n+/);
+            var instructions = (item.get('instructions') || '').split(/n+/);
+            var ingList = $('.ingredients', this);
+            var insList = $('.instructions', this);
             $('.title', this).text(item.get('title'));
+            for (var i = 0; i < ingredients.length; i++) {
+                ingList.append($('<li>').text(ingredients[i]));
+            }
+            for (var i = 0; i < instructions.length; i++) {
+                insList.append($('<li>').text(instructions[i]));
+            }
+
         };
+
+        var edit = $('.edit').get(0);
+        edit.render = function(item) {
+            item = item || {get: function() { return ''; }};
+            $('input[name=title]', this).val(item.get('title'));
+            $('input[name=ingredients]', this).val(item.get('ingredients'));
+            $('input[name=instructions]', this).val(item.get('instructions'));
+        };
+
+        $('button.add', list).click(function() {
+            edit.open();
+        });
+
+        edit.getTitle = function() {
+            var model = this.view.model;
+
+            if (model) {
+                return model.get('title');
+            }
+            return 'New';
+        };
+
+        $('button.add', edit).click(function() {
+            var el = $(edit);
+            var title = el.find('input[name=title]');
+            var ing = el.find('input[name=ingredients]');
+            var ins = el.find('input[name=instructions]');
+            var model = edit.model;
+
+            if (model) {
+                model.set({
+                    'title': title.val(),
+                    'ingredients': ing.val(),
+                    'instructions': ins.val()
+                });
+            }
+            else {
+                list.add({
+                    'title': title.val(),
+                    'ingredients': ing.val(),
+                    'instructions': ins.val()
+                });
+            }
+
+            edit.close();
+        });
     });
 });
